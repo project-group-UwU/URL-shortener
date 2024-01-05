59055f83e1b0bdae1dae39639190ea5d8b720326
Suhun Kim <kksshh404@gmail.com>
2024-01-04 17:57:07 -0800

    URL update from yesyeil.ca to short.yesyail.ca

--- usr/src/components/Divider.js
+++ usr/src/components/Divider.js
@@ -1,19 +1,13 @@
 import '../styles/Divider.css';
 
-function Divider(props) {
-	return (
-		<div className={`divider${props.lineNum}`} />
-	)
-}
-
 export default function Dividers() {
     return (
 		<div className='dividers'>
-			<Divider lineNum={0}/>
-			<Divider lineNum={1}/>
-			<Divider lineNum={2}/>
-			<Divider lineNum={3}/>
-			<Divider lineNum={4}/>
+			<div className={"divider0"}/>
+			<div className={"divider1"}/>
+			<div className={"divider2"}/>
+			<div className={"divider3"}/>
+			<div className={"divider4"}/>
 		</div>
     )
 };
